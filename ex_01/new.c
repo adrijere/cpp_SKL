55407b784336061ef9bc3db8d34038f61790db14
namaneo <namaneo@epitech.eu>
2015-01-10 15:41:52 +0100

    ex_1

---
+++ ex_01/new.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include "point.h"
+
+void *new(Class *class)
+{
+  Class *tmp;
+
+  tmp = malloc(class->__size__);
+  memcpy(tmp, class, class->__size__);
+  tmp->__init__(tmp);
+  return (tmp);
+}
+
+void delete(Object *ptr)
+{
+  Class *tmp;
+
+  tmp = (Class *)ptr;
+  tmp->__del__(tmp);
+}
+
